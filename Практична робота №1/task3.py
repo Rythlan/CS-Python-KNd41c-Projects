f650d852bd937d15d9366ae55cc01e3a3aede0d9
Rythlan@dev <frettamdev@gmail.com>
2026-01-10 21:27:06 +0200

    fix task3.py (prac1)

--- Практична робота №1/task3.py
+++ Практична робота №1/task3.py
@@ -1,16 +1,15 @@
 def main():
     n = int(input("Введіть число N: "))
-    for i in range(n, 0, -1):
-        num = n - i + 1
+    for i in range(1, n + 1):
+        num = n
         for j in range(1, n + 1):
-            if j <= n - i:
+            if j < i:
                 print(" ", end=" ")
             else:
                 print(num, end=" ")
-                num += 1
+                num -= 1
         print("")
 
 
 if __name__ == "__main__":
     main()
-
